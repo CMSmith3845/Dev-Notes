07982e6e38b812ee7c0d7c34bd9a81cd36a26cc7
CMSmith3845 <CMSmith3845@gmail.com>
2024-06-17 12:06:27 -0400

    String_Indexing

---
+++ String_Indexing.py
@@ -0,0 +1,15 @@
+course = "Python Programming"
+# This returns the length of the original string
+print(len(course))
+# This returns the first character of the string
+print(course[0])
+# This returns the last character of the string
+print(course[-1])
+# This returns the first 3 characters of the string because P is the 0 index and the 3 is the ending character but its not included
+print(course[0:3])
+# This returns the whole string cause there is no end argument
+print(course[0:])
+# This will return up to the 3rd character, so PYT again
+print(course[:3])
+# This will return a copy of the original string
+print(course[:])

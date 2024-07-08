1d6a547fcd43eac4c7922b0a621d11e8454bd406
CMSmith3845 <CMSmith3845@gmail.com>
2024-07-08 11:36:30 -0400

    updating notes and lessons

---
+++ Code with Mosh Projects/xxargs.py
@@ -0,0 +1,9 @@
+def save_user(**user):  # This will be used to save a user's information
+    print(user)
+# When using the double asterisks up top you can use multiple arguements or keyword arguements and python will turn them
+# into a dictionary
+
+
+# These are keyword arguements like in lesson default_arguements by=1
+save_user(id=1, name="John", age=22)
+# This will create a dictionary thats surrouded by curly braces

8d8f036e30a6bdab6d98f1b197965de6c3e36173
CMSmith3845 <CMSMith3845@gmail.com>
2024-07-25 00:09:32 -0400

    adding an excercise

---
+++ LeetCode Coding Problems/removing_characters_from_a_string.py
@@ -0,0 +1,10 @@
+word = input('Enter a Word ')
+num = input('Enter a Number ')
+converted_num = int(num)
+
+
+def splitting_string(input):
+    print(word[0:converted_num])
+    
+
+splitting_string(input)
